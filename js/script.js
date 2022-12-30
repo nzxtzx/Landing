899c11635412b0eaf674ada53401351671ece937
nzxtzx <96650884+nzxtzx@users.noreply.github.com>
2022-12-30 06:40:52 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -23,5 +23,59 @@
 
         });
 
+        const nav = document.querySelector('.nav');
+        const logo = document.querySelector('.nav-logo');
+
+        const onScroll = () => {
+        
+          const scroll = document.documentElement.scrollTop
+        
+          if (scroll > 0) {
+            nav.classList.add("scrolled");
+            logo.classList.add("colored");
+
+
+          } else {
+            nav.classList.remove("scrolled");
+            logo.classList.remove("colored");
+          }
+
+          if (nav.classList.contains("scrolled")){
+            menuButton.addEventListener('click', function(){
+                nav.classList.toggle("changed")
+            })
+        }
+        }
+        
+        window.addEventListener('scroll', onScroll);
+
+
+        let links = document.querySelectorAll('body a[href*="#"]');
+
+        for (link of links){
+
+            if(link){
+
+                
+            if (menuButton.classList.contains('is-active')){
+                menu.classList.remove('is-active')
+                menuList.classList.remove('is-active')
+                nav.classList.remove("scrolled")
+            }
+            
+                link.addEventListener('click', function(e){
+                    e.preventDefault();
+                    linkId = this.getAttribute('href');
+                    console.log(linkId)
+
+                    document.querySelector(linkId).scrollIntoView({
+                        behavior: "smooth",
+                        block: "start"
+                    });
+                });
+            }
+
+        }
+
 
 /* nav, menu */
